89a9c487f677ac3f82d2b7a126d932159905220e
sanryuu03 <ryuuosku@gmail.com>
2022-07-01 16:03:40 +0700

    backend admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackendAdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/admin', [BackendAdminController::class, 'index'])->middleware(['auth'])->name('index.admin');
+
 
 require __DIR__.'/auth.php';
