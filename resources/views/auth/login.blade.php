1b921026ee15048810c2ae313923da0f4e9437f5
sanryuu03 <ryuuosku@gmail.com>
2022-07-05 16:21:18 +0700

    link register di login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,6 +32,11 @@
                 </label>
             </div>
 
+            <div class="flex items-center justify-start mt-4">
+                    <a class="underline text-sm text-slate-300 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Register ?') }}
+                    </a>
+            </div>
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
